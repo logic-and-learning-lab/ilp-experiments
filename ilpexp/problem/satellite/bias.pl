bb96db4760330e8b4561377205df8a042b23cc15
Andrew Cropper <andrew.cropper@gmail.com>
2024-03-14 10:32:14 +0000

    added types and constants - still no solution

--- ilpexp/problem/satellite/bias.pl
+++ ilpexp/problem/satellite/bias.pl
@@ -1,5 +1,3 @@
-
-
 head_pred(fault,1).
  
 body_pred(succ,2).
@@ -34,38 +32,85 @@ body_pred(tm001_eod_relay,2).
 body_pred(tm211_bus_voltage,2).
 body_pred(gmt_mission_phase,2).
 
+body_pred(P,1):-
+    constant(P, _).
+
+constant(presolstice,t2).
+constant(reconditioning,t2).
+constant(solstice,t2).
+constant(eclipse,t2).
+
+constant(c_positive,t1).
+constant(c_negative,t1).
+
+type(P, (T,)):-
+    constant(P, T).
+
+type(fault,(t0,)).
+type(succ,(t0,t0)).
+type(tm040_switch,(t1,t0)).
+type(tm018_switch,(t1,t0)).
+type(gmt_mission_phase,(t2,t0)).
+type(tm001_eod_relay,(t1,t0)).
+type(tm002_battov_temp,(t1,t0)).
+type(tm004_eoc_signaled,(t1,t0)).
+type(tm007_switch,(t1,t0)).
+type(tm009_switch,(t1,t0)).
+type(tm011_eod_override,(t1,t0)).
+type(tm013_switch,(t1,t0)).
+type(tm015_eod_signaled,(t1,t0)).
+type(tm017_switch,(t1,t0)).
+type(tm021_eoc_disabled,(t1,t0)).
+type(tm022_switch,(t1,t0)).
+type(tm029_ovt_disabled,(t1,t0)).
+type(tm031_switch,(t1,t0)).
+type(tm038_switch,(t1,t0)).
+type(tm039_eod_disabled,(t1,t0)).
+type(tm042_switch,(t1,t0)).
+type(tm043_switch,(t1,t0)).
+type(tm054_supply_1a,(t1,t0)).
+type(tm055_supply_1b,(t1,t0)).
+type(tm057_supply_2c,(t1,t0)).
+type(tm058_asr_or_switch_10,(t1,t0)).
+type(tm070_supply_3c,(t1,t0)).
+type(tm071_asr_or_switch_20,(t1,t0)).
+type(tm211_bus_voltage,(t1,t0)).
+type(tm220_supply_1c,(t1,t0)).
+type(tm222_charging,(t1,t0)).
+type(tm257_battery_voltage,(t1,t0)).
 
 
-direction(fault,(in,)).
- 
-direction(succ,(out,in)).
-direction(tm040_switch,(out,in)).
-direction(tm018_switch,(out,in)).
-direction(tm031_switch,(out,in)).
-direction(tm038_switch,(out,in)).
-direction(tm022_switch,(out,in)).
-direction(tm043_switch,(out,in)).
-direction(tm013_switch,(out,in)).
-direction(tm042_switch,(out,in)).
-direction(tm007_switch,(out,in)).
-direction(tm222_charging,(out,in)).
-direction(tm071_asr_or_switch_20,(out,in)).
-direction(tm070_supply_3c,(out,in)).
-direction(tm058_asr_or_switch_10,(out,in)).
-direction(tm057_supply_2c,(out,in)).
-direction(tm257_battery_voltage,(out,in)).
-direction(tm017_switch,(out,in)).
-direction(tm009_switch,(out,in)).
-direction(tm220_supply_1c,(out,in)).
-direction(tm055_supply_1b,(out,in)).
-direction(tm054_supply_1a,(out,in)).
-direction(tm029_ovt_disabled,(out,in)).
-direction(tm021_eoc_disabled,(out,in)).
-direction(tm004_eoc_signaled,(out,in)).
-direction(tm002_battov_temp,(out,in)).
-direction(tm039_eod_disabled,(out,in)).
-direction(tm015_eod_signaled,(out,in)).
-direction(tm011_eod_override,(out,in)).
-direction(tm001_eod_relay,(out,in)).
-direction(tm211_bus_voltage,(out,in)).
-direction(gmt_mission_phase,(out,in)).
+
+%% direction(fault,(in,)).
+
+%% direction(succ,(out,in)).
+%% direction(tm040_switch,(out,in)).
+%% direction(tm018_switch,(out,in)).
+%% direction(tm031_switch,(out,in)).
+%% direction(tm038_switch,(out,in)).
+%% direction(tm022_switch,(out,in)).
+%% direction(tm043_switch,(out,in)).
+%% direction(tm013_switch,(out,in)).
+%% direction(tm042_switch,(out,in)).
+%% direction(tm007_switch,(out,in)).
+%% direction(tm222_charging,(out,in)).
+%% direction(tm071_asr_or_switch_20,(out,in)).
+%% direction(tm070_supply_3c,(out,in)).
+%% direction(tm058_asr_or_switch_10,(out,in)).
+%% direction(tm057_supply_2c,(out,in)).
+%% direction(tm257_battery_voltage,(out,in)).
+%% direction(tm017_switch,(out,in)).
+%% direction(tm009_switch,(out,in)).
+%% direction(tm220_supply_1c,(out,in)).
+%% direction(tm055_supply_1b,(out,in)).
+%% direction(tm054_supply_1a,(out,in)).
+%% direction(tm029_ovt_disabled,(out,in)).
+%% direction(tm021_eoc_disabled,(out,in)).
+%% direction(tm004_eoc_signaled,(out,in)).
+%% direction(tm002_battov_temp,(out,in)).
+%% direction(tm039_eod_disabled,(out,in)).
+%% direction(tm015_eod_signaled,(out,in)).
+%% direction(tm011_eod_override,(out,in)).
+%% direction(tm001_eod_relay,(out,in)).
+%% direction(tm211_bus_voltage,(out,in)).
+%% direction(gmt_mission_phase,(out,in)).
